a6b35e8a338408800db796c10dcb64397835a2ea
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-09-16 14:59:34 -0700

    Add files via upload

---
+++ check.py
@@ -0,0 +1,18 @@
+n1, n2 = 0, 1
+count = 0
+
+n = int(input("enter the number for fibonacci"))
+
+if n<= 0:
+    print("wrong value")
+elif n == 1:
+    print(n1)
+    print(n2)
+else:
+    while count < n:
+        
+        n3 = n1 +n2
+        n1 = n2
+        n2 = n3
+        count = count + 1
+        print(n3)
