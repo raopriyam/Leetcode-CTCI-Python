f515c4581c71e5e5f158ee9acc1f9f1036689f20
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-02 20:23:30 -0700

    Add files via upload

---
+++ reverseString.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  2 20:16:54 2021
+
+@author: priya
+"""
+
+def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # count = len(s)-1
+        # for i in range(len(s)):
+        #     a = s.pop(count)
+        #     s.insert(i,a)
+        # return s
+        return s.reverse()
