79a62c44febf780a35d258bd0133cdc7ff3f4c7e
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-12 22:26:21 -0700

    Add files via upload

--- oneWay.py
+++ oneWay.py
@@ -6,29 +6,30 @@ Created on Mon Oct 11 18:09:46 2021
 """
 
 def oneAway(s1,s2):
-    ans = 0
-    dict1 = {}
     count = 0
-    if len(s1) >= len(s2):
-        for i in s1:
-            dict1[i] = count
-            count += 1
-        for i in s2:
-            if i not in dict1:
-                ans += 1
+    if len(s1) == len(s2):
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                count += 1
+    elif len(s1) == len(s2)-1:
+        count = 1
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                count += 1
+    elif len(s2) == len(s1)-1:
+        count = 1
+        for i in range(len(s2)):
+            if s1[i] != s2[i]:
+                count += 1
+                
     else:
-        for i in s2:
-            dict1[i] = count
-            count += 1
-        for i in s1:
-            if i not in dict1:
-                ans += 1
+        return -1
         
-    return ans
+    return count
 
-a = oneAway("Priyam","Priy")
+a = oneAway("Priyam","Priya")
 print(a)
-if a>1:
-    print(False)
-else:
+if a==0 or a==1:
     print(True)
+else:
+    print(False)
