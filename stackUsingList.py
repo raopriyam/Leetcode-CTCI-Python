c60ad7572f6f107d3ce2daed2f35e6e9018f99c5
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-14 22:54:47 -0700

    Add files via upload

---
+++ stackUsingList.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 22:22:00 2021
+
+@author: priya
+"""
+
+ 
+stack = []
+ 
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+
+print(stack)
+
+print(stack.pop())
+print(stack)
+
+print(stack.pop())
+print(stack)
+ 
