67c323532f615c382951e672424eb6f7a3da8842
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-16 22:39:05 -0700

    Add files via upload

---
+++ IntersectionOfLinkedList.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 17:50:40 2021
+
+@author: priya
+"""
+
+class ListNode:
+   def __init__(self, data, next = None):
+      self.data = data
+      self.next = next
+class Solution(object):
+   def getIntersectionNode(self, headA, headB):
+      """
+      :type head1, head1: ListNode
+      :rtype: ListNode
+      """
+      dict = {}
+      while headA:
+         dict[headA]=1
+         headA = headA.next
+      while headB:
+         if headB in dict:
+            return headB
+         headB = headB.next
+      return None
+headA = ListNode(4)
+headB = ListNode(5)
+Intersect = ListNode(8, ListNode(4, ListNode(5)))
+headA.next = ListNode(1, Intersect)
+headB.next = ListNode(0, ListNode(1, Intersect))
+ob1 = Solution()
+op = ob1.getIntersectionNode(headA, headB)
+print("Intersection:",op.data)
