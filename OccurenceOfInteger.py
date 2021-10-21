22675f30a604e355955420d05a12643eb5f09de6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-20 21:56:47 -0700

    Add files via upload

---
+++ OccurenceOfInteger.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 02:57:53 2021
+
+@author: priya
+"""
+
+def OccurenceOfInteger(arr,n):
+    ans = arr.count(n)
+    return ans
+
+print(OccurenceOfInteger([1,2,3,1,4,2,3,5,4,67,5,1,4,3],-2))
