be26677f47285d74911d6b24df688c6d5585ebe6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-20 21:57:34 -0700

    Add files via upload

---
+++ moveNegativeOnEnd.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 23:43:20 2021
+
+@author: priya
+"""
+
+def moveNegativeOnEnd1(arr):
+    arr.sort(reverse= True)
+    return arr
+    
+
+        
+        
+    
+
+print(moveNegativeOnEnd1([1,2,-3,4,-5,-6,7,-8,9]))
+
+print(moveNegativeOnEnd1([1, -1, 3, 2, -7, -5, 11, 6]))
+    
+
