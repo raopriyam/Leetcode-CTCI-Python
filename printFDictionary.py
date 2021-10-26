7024069f9bb472c2e667fad173ebe5c40cb900c8
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-25 23:15:30 -0700

    Add files via upload

---
+++ printFDictionary.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 22:25:38 2021
+
+@author: priya
+"""
+
+def printdict1(dict1):
+    for i in dict1:
+        print(i,dict1[i])
+        
+dict1={1:1,2:2,3:3}
+printdict1(dict1)
