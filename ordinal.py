5d9c75562bfede4f173066a6b34ab2b2501a4155
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-28 22:58:49 -0700

    Add files via upload

---
+++ ordinal.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 17:00:51 2021
+
+@author: priya
+"""
+
+def ordinal(val):
+    return ord(val)
+
+
+print(ordinal("1"))
+print(ordinal("2"))
+print(ordinal("3"))
+print(ordinal("4"))
+print(ordinal("5"))
