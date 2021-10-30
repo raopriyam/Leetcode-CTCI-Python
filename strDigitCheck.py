ab996c1e237148ff29373d4f593635cd027fc8f7
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-10-29 23:55:35 -0700

    Add files via upload

---
+++ strDigitCheck.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 22:06:40 2021
+
+@author: priya
+"""
+
+def strcheck(s):
+    return s.isdigit()
+
+print(strcheck("priyam"))
+print(strcheck("rao"))
+print(strcheck("12345"))
+print(strcheck("priyamrao123"))
+print(strcheck("187426123"))
