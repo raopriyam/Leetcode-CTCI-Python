7b58c84ff74c388414a1b04fbd1fd0f2d110a8a5
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-06 23:59:59 -0700

    Add files via upload

---
+++ printList3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  6 23:58:46 2021
+
+@author: priya
+"""
+
+def printList3(list1):
+    for i in list1:
+        print(i)
+        
+printList3([1,2,3,4,5])
