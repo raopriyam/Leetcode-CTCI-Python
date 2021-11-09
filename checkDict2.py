343e05c582a986fb4d88fd1752950420cba9e982
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-08 23:59:25 -0700

    Add files via upload

---
+++ checkDict2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 23:52:51 2021
+
+@author: priya
+"""
+
+def checkDict1(dict1):
+    for i in dict1:
+        print(i," :",dict1[i])
+    
+checkDict1({"a":1,"b":2,"c":3})
+        
