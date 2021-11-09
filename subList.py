06f2a2e35848c8832cb17b5b5bf8af99eb8c2a51
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-08 23:59:53 -0700

    Add files via upload

---
+++ subList.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 23:57:27 2021
+
+@author: priya
+"""
+
+def subList(list1):
+    for i in list1:
+        i = i-1
+    print(list1)
+
+subList([1,2,3,4,5])
