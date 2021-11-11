479fbcd35c9610358b4ab706113b07fafe1503ac
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-10 23:19:06 -0700

    Add files via upload

---
+++ stringConvertArray.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 23:09:55 2021
+
+@author: priya
+"""
+
+def stringconvertArray(s):
+    return list(s)
+
+print(stringconvertArray("priyam"))
+print(stringconvertArray("my name is priyam"))
