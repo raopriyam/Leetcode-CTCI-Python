f5a465689dc1158759bedbe5c61aef29d14a4b10
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-11 23:39:55 -0700

    Add files via upload

---
+++ setFunc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 23:28:44 2021
+
+@author: priya
+"""
+
+def setFunc(set1):
+    for i in set1:
+        print(i,end=" ")
+        
+        
+setFunc({0,1,2,3,4,5,6,7,8,9})
