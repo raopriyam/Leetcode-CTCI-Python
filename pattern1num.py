cdb3c0cb0a3bd2882c89d30a8c121b2da56bea94
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-18 23:29:52 -0700

    Add files via upload

---
+++ pattern1num.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 22:32:28 2021
+
+@author: priya
+"""
+
+def pattern1num(n):
+    for i in range(n):
+        print("*")
+        
+pattern1num(10)
