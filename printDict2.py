6d738cd3cd24319c5df1905853013a8443aeb46c
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-20 23:58:53 -0700

    Add files via upload

---
+++ printDict2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 23:54:18 2021
+
+@author: priya
+"""
+
+def printDict2(dict1):
+    for i in dict1:
+        print(i,":",dict1[i])
+        
+printDict2({"1":"a","2":"b","3":"c"})
