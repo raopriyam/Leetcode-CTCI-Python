728b697b0700fa9d4aede3bbbe428c0e11ff3c83
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-20 23:59:09 -0700

    Add files via upload

---
+++ check2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 23:56:23 2021
+
+@author: priya
+"""
+
+def check2(arr):
+    if arr:
+        return True
+    else:
+        return False
+    
+print(check2([1,2,3,4,5]))
+
+print(check2([]))
