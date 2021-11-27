454ee398683ad86154eaec057723756634d28245
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-26 23:51:39 -0700

    Add files via upload

---
+++ strCheck.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 23:41:24 2021
+
+@author: priya
+"""
+
+def strCheck(s):
+    if s:
+        print(s)
+    else:
+        print("Wrong Input")
+        
+strCheck("Priyam")
+strCheck("")
+strCheck("")
