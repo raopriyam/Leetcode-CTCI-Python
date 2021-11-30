50fc301e28172bd062de05920481dba69280d268
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-11-29 23:32:59 -0700

    Add files via upload

---
+++ createArray.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 23:20:59 2021
+
+@author: priya
+"""
+
+def createArray(n):
+    list1 = []
+    for i in range(n):
+        list1.append(i)
+    return list1
+
+
+print(createArray(5))
+        
+    
