201c5b6a0247dcf784d73046993f707ac3cd2a65
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-08 23:59:28 -0700

    Add files via upload

---
+++ printString.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 23:52:48 2021
+
+@author: priya
+"""
+
+def printString(s):
+    for i in s:
+        print(i,end= " ")
+        
+printString("My name is Priyam Rao")
