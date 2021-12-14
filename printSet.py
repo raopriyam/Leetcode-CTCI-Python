8d885b38b2380fb27f2688a690b84738792af33c
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-14 12:11:01 -0700

    Add files via upload

---
+++ printSet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 22:22:59 2021
+
+@author: priya
+"""
+
+def printSet(set1):
+    for i in set1:
+        print(i,end=" ")
+        
+printSet({1,2,3,4,5,6,7,8,9})
