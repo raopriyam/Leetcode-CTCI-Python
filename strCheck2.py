086c84d8e58dfe30a1d17e2127afa15b1922135f
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-14 23:49:04 -0700

    Add files via upload

---
+++ strCheck2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 22:24:20 2021
+
+@author: priya
+"""
+
+def strcheck(s):
+    return s.isalpha()
+        
+print(strcheck("Priyam"))
+print(strcheck("Priyam1"))
+print(strcheck("Priyam"))
+print(strcheck("Priyam*"))
