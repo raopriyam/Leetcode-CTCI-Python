19ed1dfad8f36d6e3ad5305f4aba52854af5d590
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-15 23:54:44 -0700

    Add files via upload

---
+++ check4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 23:39:17 2021
+
+@author: priya
+"""
+
+def check4(set1):
+    for i in set1:
+        print("val ",i,": ",i)
+        
+check4({1,2,3,4,5})
