ce069da2400320c0897dc4aed7d8999ac7e6b7bf
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-15 23:55:27 -0700

    Add files via upload

---
+++ printDict4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 23:47:32 2021
+
+@author: priya
+"""
+
+def printDict4(dict1):
+    for i in dict1:
+        print(i,":",dict1[i])
+        
+printDict4({"1":"a","2":"b"})
