caffb421c3e30365a47306136844b76968ea95cc
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2021-12-15 23:54:30 -0700

    Add files via upload

---
+++ check3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 23:31:20 2021
+
+@author: priya
+"""
+
+def check():
+    set1 = set()
+    set1.add(1)
+    set1.add(2)
+    set1.add(3)
+    return set1 
+
+print(check())
+    
