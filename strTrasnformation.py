eaefa0edcd9cffcf5d15019573def12b1d4807c6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-06-23 00:08:02 -0500

    Add files via upload

---
+++ strTrasnformation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 21:00:26 2022
+
+@author: priya
+"""
+
+str1 = "Priyam Rao"
+str2 = str1.upper()
+str3 = str1.lower()
+print(str3.isupper())
+print(str2.islower())
+print(str3.islower())
+print(str2.isupper())
+print((str3+str2).isupper())
+print((str3+str2).islower())
