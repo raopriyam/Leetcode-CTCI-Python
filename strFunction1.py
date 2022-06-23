871c987993fac10efd6769345218e60fbe264283
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-06-23 00:07:34 -0500

    Add files via upload

---
+++ strFunction1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 20:51:11 2022
+
+@author: priya
+"""
+
+str1 = "priyam rao"
+str2 = str1.split(" ")
+str3 = str1.replace("r","?")
+str4 = str1.strip('o')
+print(str2)
+print(str3)
+print(str4)
