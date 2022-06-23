a48f4a507cb03810f0d506cbc46f0fe5ef46b53c
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-06-23 00:08:29 -0500

    Add files via upload

---
+++ mathOp1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 21:04:18 2022
+
+@author: priya
+"""
+
+from math import *
+
+num = -5
+print(abs(num))
+print(max(num,num-4))
+print(min(num,num+4))
+print(pow(num,num))
+print(round(num))
+print(ceil(1.34))
+print(floor(2.5))
+print(sqrt(1221))
