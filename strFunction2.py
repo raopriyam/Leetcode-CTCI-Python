cf93d1a14b2c21dc69c90b35f5a734f5b79bf4c0
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-06-23 00:07:50 -0500

    Add files via upload

---
+++ strFunction2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 20:54:57 2022
+
+@author: priya
+"""
+
+str1 = "priyam rao"
+str2 = str1.lower()
+str3 = str1.upper()
+str4 = " ".join(str2)
+print(str2,str3,str4)
