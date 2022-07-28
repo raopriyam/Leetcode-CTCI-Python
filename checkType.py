39d55b097cfcee88347b0400273cfd1b10e6af78
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2022-07-28 00:16:07 -0500

    Add files via upload

---
+++ checkType.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 22:06:07 2022
+
+@author: priya
+"""
+
+def checkType(inp1):
+    return type(inp1)
+
+    
+print(checkType("Priyam"))
+print(checkType(12346))
+print(checkType("Priyam123"))
+print(checkType(True))
