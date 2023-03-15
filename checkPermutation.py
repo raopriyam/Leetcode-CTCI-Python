bd2788339dfd5e782edebfaa7d704b8424b37754
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-14 23:42:24 -0700

    Add files via upload

--- checkPermutation.py
+++ checkPermutation.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 11 13:14:27 2021
+Created on Tue Mar 14 22:49:08 2023
 
 @author: priya
 """
 
-def checkPermutation(s1,s2):
-    if len(s1)!=len(s2):
-        return False
-    else:
-        a1 = list(s1)
-        a2 = list(s2)
-        a1.sort()
-        a2.sort()
-    if a1 == a2:
+def checkPermutation(string1,string2):
+    arr1 = list(string1)
+    arr2 = list(string2)
+    arr1.sort()
+    arr2.sort()
+    if arr1 == arr2:
         return True
-    return False
+    else: 
+        return False
+    
 
-print(checkPermutation("Priyam","Priyam"))
-print(checkPermutation("PriyaM","Priyam"))
-print(checkPermutation("PrIYam","Priyam"))
-print(checkPermutation("Priyamrao","Priyamrao"))
+print(checkPermutation("Priyam", "mariP"))
+    
+    
