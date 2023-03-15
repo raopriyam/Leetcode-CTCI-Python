58866856e08a1ed2a8d73ecfe4ec3c3881a5ff8e
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-14 23:43:18 -0700

    Add files via upload

---
+++ Urlify2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 23:01:39 2023
+
+@author: priya
+"""
+
+def Urlify2(string):
+    return string.replace(" ", "%")
+
+
+print(Urlify2("priyam Rao"))
