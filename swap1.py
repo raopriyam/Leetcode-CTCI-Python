d408544589042048163e1b00fc40d50617e88d3b
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-22 23:49:41 -0700

    Add files via upload

---
+++ swap1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 23:01:03 2023
+
+@author: priya
+"""
+
+num1 = 10
+num2 = 20
+
+def swap1(num1,num2):
+    temp = num1
+    num1= num2
+    num2 = temp
+    print("num1=",num1,"num2=", num2)
+
+
+swap1(num1, num2)
+    
---
+++ swap2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 23:04:01 2023
+
+@author: priya
+"""
+
+num1 = 10
+num2 = 20
+
+def swap2(num1,num2):
+    num1 = num1 + num2
+    num2 = num1 - num2
+    num1 = num1- num2
+    print("num1=",num1,"num2=",num2)
