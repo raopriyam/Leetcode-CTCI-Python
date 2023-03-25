84f2ff0d0532993b86b94de166a55fe865856eff
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-24 23:27:06 -0700

    Add files via upload

---
+++ reverseInt.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 23:22:43 2023
+
+@author: priya
+"""
+
+def reverseInt(n):
+    return int(str(n)[::-1])
+
+print(reverseInt(123))
--- reverseString.py
+++ reverseString.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct  2 20:16:54 2021
+Created on Fri Mar 24 23:14:57 2023
 
 @author: priya
 """
 
-def reverseString(self, s):
-        """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
-        """
-        # count = len(s)-1
-        # for i in range(len(s)):
-        #     a = s.pop(count)
-        #     s.insert(i,a)
-        # return s
-        return s.reverse()
+def reverseString(string):
+    return string[::-1]
+
+print(reverseString("priyam"))
