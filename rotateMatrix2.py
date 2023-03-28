3b90261e227e17a64aebc8f80e40f7dcaf39ae81
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-27 23:07:16 -0700

    Add files via upload

--- rotateMatrix2.py
+++ rotateMatrix2.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 16 23:37:14 2022
+Created on Mon Mar 27 22:55:19 2023
 
 @author: priya
 """
 
-def rotateMatrix2(mat):
-    
-    for i in range(len(mat)):
-        for j in range(i):
-            mat[i][j],mat[j][i] = mat[j][i],mat[i][j]
-    return mat
+def rotateMatrix2(matrix):
+    print(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            matrix[i],matrix[j] = matrix[j],matrix[i]
+    return matrix
 
 print(rotateMatrix2([[1,2,3],[4,5,6],[7,8,9]]))
