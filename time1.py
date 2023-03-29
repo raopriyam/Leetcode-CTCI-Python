be68eec5a92f7fe4fd1754b36e8f3b1b8aae008b
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-28 22:29:01 -0700

    Add files via upload

---
+++ time1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 22:15:51 2023
+
+@author: priya
+"""
+
+import time
+
+start = time.time()
+print("Execution")
+sleep = time.sleep(2)
+end = time.time()
+
+print("the time required in execution is ",end - start)
