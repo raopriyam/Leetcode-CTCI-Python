de7a31e9950a5efb9fbcdf8c780e32f607513d97
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-28 22:29:16 -0700

    Add files via upload

---
+++ colorText.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 22:15:51 2023
+
+@author: priya
+"""
+
+import colorama
+print(colorama.Fore.RED + "PRINT ME RED")
+print(colorama.Fore.GREEN + "PRINT ME GREEN")
+print(colorama.Style.RESET_ALL)  
