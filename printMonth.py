18e9a8cb943390ee46b109d8128cc18548a9dc72
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-03-30 22:34:32 -0700

    Add files via upload

---
+++ printMonth.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 22:15:58 2023
+
+@author: priya
+"""
+
+import calendar
+
+def printMonth(year,month):
+    return calendar.month(year,month)
+
+print(printMonth(2023, 3))
+    
