532fd95c0b2bca180a7583b66b9141c1fa9b9510
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-01 23:33:43 -0700

    Add files via upload

---
+++ InsertAtTheEndLinkedList.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 22:49:50 2023
+
+@author: priya
+"""
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+head = Node("head")
+secondNode = Node("second node")
+tail = Node("tail")
+
+head.next = secondNode
+secondNode.next = tail
+
+newNode = Node("new node")
+
+def insertNodeAtTheEnd(head,newNode):
+    currentNode = head
+    while currentNode.next != None:
+        currentNode = currentNode.next
+    currentNode.next = newNode
+    currentNode.next.next = None
+
+def printLinkedList(head):
+    currentNode = head
+    while currentNode != None:
+        print(currentNode.data,end = " -> ")
+        currentNode = currentNode.next
+    print("None")
+    
+printLinkedList(head)
+insertNodeAtTheEnd(head, newNode)
+printLinkedList(head)
