572b20006ea1fa7bc31bfbd0cb146604fb8b36a2
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-02 23:23:46 -0700

    Add files via upload

---
+++ sortString.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 23:10:04 2023
+
+@author: priya
+"""
+
+def sortString(string):
+    return ''.join(sorted(string))
+
+
+print(sortString("priyam"))
