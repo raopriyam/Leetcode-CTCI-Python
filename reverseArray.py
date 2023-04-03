3f02b47a36f54d82d1d55c3b9b49ee30bb3aaad4
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-02 23:23:59 -0700

    Add files via upload

--- reverseArray.py
+++ reverseArray.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 29 23:25:40 2021
+Created on Sun Apr  2 23:11:31 2023
 
 @author: priya
 """
 
-def reverseArray(arr):
-    return arr[::-1]
+def reverseArray(list1):
+    return list1[::-1]
 
-print(reverseArray([0,1,2,3,4,5]))
+print(reverseArray([1,2,3,4,5,6,7]))
