a88bee796f2358d395e9794d02bfe110adc05e4d
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-03 23:26:48 -0700

    Add files via upload

---
+++ Odd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 23:18:58 2023
+
+@author: priya
+"""
+
+def Odd(number):
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+        
+Odd(11)
+Odd(18)
