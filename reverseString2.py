cb175428ad120eed282c400092c0b41f329c5c03
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-03 23:28:03 -0700

    Add files via upload

--- reverseString2.py
+++ reverseString2.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 10 22:44:53 2021
+Created on Mon Apr  3 23:20:31 2023
 
 @author: priya
 """
 
-def stringprintK(s):
-    strn = ""
-    j = 0
-    for i in range(0,len(s)+1,2):
-        if i%4 != 0:
-            a = s[j:i]
-            strn = strn + a[::-1]
-        if i%4 == 0:
-            strn = strn + s[j:i]
-        j = i
-    #strn = strn + s[j:i]
-    return strn 
-
-print(stringprintK("abcdefgh"))
-        
+def reverseString(string):
+    reverse_string = string[::-1]
+    print(f"The reversed string is {reverse_string}.")
+    
+reverseString("priyam Rao")
