4526160e2a19177b99da4fc37a59ae6f09b116d6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-03 23:26:13 -0700

    Add files via upload

---
+++ areaRectangle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 23:16:50 2023
+
+@author: priya
+"""
+
+def areaRectangle(length,width):
+    area = length * width
+    print(f"The area is {area} square units.")
+    
+    
+areaRectangle(3, 4)
+
