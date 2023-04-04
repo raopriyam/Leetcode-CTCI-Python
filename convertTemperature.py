06482d318332869c21e6e45e17c8408ddd20238f
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-03 23:27:03 -0700

    Add files via upload

---
+++ convertTemperature.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 23:18:30 2023
+
+@author: priya
+"""
+
+def convertTemperature(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"The temperature in Fahrenheit is {fahrenheit}.")
+    
+    
+convertTemperature(37.5)
