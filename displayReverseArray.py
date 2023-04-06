cb132f82048ffd0a230bb6b756b0832a2bfa7e88
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-05 23:39:29 -0700

    Add files via upload

---
+++ displayReverseArray.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 23:32:05 2023
+
+@author: priya
+"""
+
+def displayReverseArray(arr):
+    return arr[::-1]
+
+print(displayReverseArray([1,2,3,4,5]))
