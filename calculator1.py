22eeadc26ca4c1eac1c315ec4d0fc9a6662ed356
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-07 22:40:11 -0700

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 22:17:55 2023
+
+@author: priya
+"""
+
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+
+print("Addition: ",num1+num2)
+print("Subtraction: ",num1-num2)
+print("Multiplication: ",num1*num2)
+print("Division: ",num1//num2)
+
