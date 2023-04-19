c9816a7acfc62bcd3b9946749030a4647b9ab1d8
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-04-18 22:27:33 -0700

    Add files via upload

---
+++ kilometerIntoMiles.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 22:24:35 2023
+
+@author: priya
+"""
+
+def kiloMetersIntoMiles(kms):
+    return kms*(0.621371)
+
+print("the miles for given distnce in kilometers are",kiloMetersIntoMiles(23.45))
