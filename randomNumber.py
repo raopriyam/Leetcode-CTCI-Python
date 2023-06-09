88e2db7382b164081fff2730af163842a7daa6ff
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-06-08 20:08:00 -0600

    Add files via upload

---
+++ randomNumber.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 18:54:35 2023
+
+@author: priya
+"""
+
+import random
+
+def randomNumber():
+    return random.randint(1,10000)
+
+print(randomNumber())
