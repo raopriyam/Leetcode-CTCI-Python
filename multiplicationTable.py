6ce809d43c95f99cf334aae8757481e27dafe329
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-06-08 20:09:08 -0600

    Add files via upload

---
+++ multiplicationTable.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 18:58:10 2023
+
+@author: priya
+"""
+
+def multiplicationTable(n,k):
+    for i in range(1,k+1):
+        print(n," x ", i, " = ", n*i)
+        
+multiplicationTable(5,24)
+
+
+        
