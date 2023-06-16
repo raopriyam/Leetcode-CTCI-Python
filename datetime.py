f4cf147c19a378e29b713ecd60e3ab42568bad57
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-06-16 17:11:48 -0600

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 16:01:06 2023
+
+@author: priya
+"""
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
