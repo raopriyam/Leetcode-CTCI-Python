20ff2024355dc05ee8d41db3a607265617580290
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-06-16 17:12:20 -0600

    Add files via upload

---
+++ ListTuples.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 16:03:12 2023
+
+@author: priya
+"""
+
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
