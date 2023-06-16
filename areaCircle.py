3c25dbb1a40074fa4b24fa94872617f5d4b7c825
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-06-16 17:12:03 -0600

    Add files via upload

---
+++ areaCircle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 16:02:21 2023
+
+@author: priya
+"""
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
