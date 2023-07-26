5bbdf58112e6defbb281cdb7ad48cc1335e099cb
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-07-25 23:17:11 -0700

    Add files via upload

---
+++ celciousToFarenheit.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 23:08:31 2023
+
+@author: priya
+"""
+
+def celciousToFarenheit(tempCelc):
+    return (1.8 * tempCelc) + 32
+
+
+print(celciousToFarenheit(37))
+    
