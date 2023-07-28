6eeeb70b53b2229ccc05466c4158491e5d286adf
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-07-27 18:40:17 -0700

    Add files via upload

--- maximumWealth.py
+++ maximumWealth.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 17 18:02:11 2022
+Created on Thu Jul 27 17:46:37 2023
 
 @author: priya
 """
 
-def maximumWealth(arr):
-    sum1 = 0
-    sumnum = 0
-    for arr1 in arr:
-        if sum(arr1) >= sum1:
-            sum1= sum(arr1)
-            sumnum = sumnum + 1
-            
-    return sum1
+def maximumWealth(accounts):
+    """
+    :type accounts: List[List[int]]
+    :rtype: int
+    """
+    numOfCustomers = len(accounts)
+    numOfAccounts = len(accounts[0])
+    ans = [0]*numOfCustomers
+    for i in range(numOfCustomers):
+        for j in range(numOfAccounts):
+            ans[i] = ans[i] + accounts[i][j]
+    return max(ans)
 
-print(maximumWealth([[1,2,3],[2,3,1]]))
-
-print(maximumWealth([[1,5],[7,3],[3,5]]))
-            
-print(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
-            
-    
+print(maximumWealth( [[1,2,3],[1,3,2]]))
