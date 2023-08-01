9bab6c55e53cfd6a347ce8eee08ab7597152b062
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-07-31 23:12:17 -0700

    Add files via upload

---
+++ calendarMonthYear.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 23:06:19 2023
+
+@author: priya
+"""
+
+import calendar
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+print(calendar.month(y, m))
