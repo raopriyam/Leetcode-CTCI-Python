386e9a8b72007c3cf8ce2984249a8ab01b03cc38
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-07-31 23:11:31 -0700

    Add files via upload

---
+++ userInfo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 23:04:22 2023
+
+@author: priya
+"""
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
