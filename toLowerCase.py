45eb176f2397cff6d2f5ae2e7b40ba82d3d126b3
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-08-20 23:26:57 -0700

    Add files via upload

---
+++ toLowerCase.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 23:09:51 2023
+
+@author: priya
+"""
+
+def toLowerCase( s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    return s.lower()
+
+print(toLowerCase("Priyam Rao"))
