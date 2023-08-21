7e217adbc9939cd5efb8d31844f6eb808392a811
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-08-20 23:27:25 -0700

    Add files via upload

---
+++ toUpperCase.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 23:17:19 2023
+
+@author: priya
+"""
+
+def toUpperCase( s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    return s.upper()
+
+print(toUpperCase("Priyam Rao"))
