ea808736743384562ce582c75bfce8474b81be40
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2023-10-16 18:40:51 -0700

    Add files via upload

--- removeElement.py
+++ removeElement.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 27 22:12:26 2022
+Created on Sat Oct 14 18:58:23 2023
 
 @author: priya
 """
 
-def removeElement( nums, val):
-    """
-    :type nums: List[int]
-    :type val: int
-    :rtype: int
-    """
-    count= 0
-    ln = len(nums)
-    while count<ln:
-        if nums[count]==val:
-            nums.remove(nums[count])
-            ln = ln-1
-        else:
-            count = count+1
-    return nums, ln
-
-print(removeElement([1,2,3,4,2,3,5,6,2,7,8,4,2,6,4,9,0],2))
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        i = 0
+        while i<len(nums) :
+            if nums[i] == val:
+                nums.remove(val)
+                i -= 1
+            i+=1
+        return count,nums
+    
