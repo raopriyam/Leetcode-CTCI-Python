04d49950c458ca437ba85f67a12b7690ae0fa6be
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2024-12-01 23:38:45 -0800

    Add files via upload

--- arrangeCoins.py
+++ arrangeCoins.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov  6 23:34:09 2021
+Created on Fri Nov 22 14:04:13 2024
 
 @author: priya
 """
 
-def arrangeCoins(n):
+def arrangeCoins(self, n):
     """
     :type n: int
     :rtype: int
     """
     if n == 1:
         return 1
-    i = 0
-    while i<n:
-        i += 1
-        n = n-i
-        
-    return i
+    count = 1
+    result = 0
+    while n>0:
+        n = n - count
+        if n < 1:
+            return result
+        result += 1
+        count = count + 1
+    
 
-print(arrangeCoins(5))
-        
