ea2e9b4529226662b34746bca7deb8ce5bdc1195
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2024-12-04 23:54:57 -0800

    Add files via upload

---
+++ addStrings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 23:23:30 2024
+
+@author: priya
+"""
+
+def addStrings(self, num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+    return str(int(num1) + int(num2))
