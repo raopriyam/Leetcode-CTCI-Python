55a86d8816df08e44b783582a5de14c7ac10f7b6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-18 23:42:22 -0800

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 23:32:36 2025
+
+@author: priya
+"""
+
+def multi(a,b):
+    return a*b
+
+
+print(multi(2,4))
+print(multi(-2,4))
+print(multi(-2,-4))
+print(multi(1/2,4/7))
+print(multi(0,4))
