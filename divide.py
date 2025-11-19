fd9ea5743297082ed46e2fffaef015165fe6f49c
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-18 23:42:39 -0800

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 23:34:07 2025
+
+@author: priya
+"""
+
+def divide(a,b):
+    return a/b
+
+
+print(divide(6,3))
+print(divide(1,3))
+print(divide(1/6,3))
+print(divide(60,3.5))
+print(divide(6,3))
