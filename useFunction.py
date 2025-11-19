4b4e9b68c0652e3f97e3aaa2e278dca27c0bc84e
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-18 23:43:15 -0800

    Add files via upload

---
+++ useFunction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 23:35:47 2025
+
+@author: priya
+"""
+
+
+
+def useFunction(a,b):
+    print("add: ",a+b)
+    print("sub: ",a-b)
+    print("mul: ",a*b)
+    print("div",a/b)
+    
+    
+useFunction(6,3)
