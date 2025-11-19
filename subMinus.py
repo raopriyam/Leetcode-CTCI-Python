23186f2562a88656e150c165cd58bc6ca29352d9
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-18 23:42:05 -0800

    Add files via upload

---
+++ subMinus.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 23:30:22 2025
+
+@author: priya
+"""
+
+def subMinus(a,b):
+    return a - b
+
+
+print(subMinus(8,2))
+print(subMinus(8,-2))
+print(subMinus(-8,2))
+print(subMinus(-8,-2))
+print(subMinus(0,2))
