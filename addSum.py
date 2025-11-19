398d51288cd4af8add90e53bbd692b6f0e0a9155
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-18 23:42:58 -0800

    Add files via upload

---
+++ addSum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 18 23:28:56 2025
+
+@author: priya
+"""
+
+def addSum(a,b):
+    return a+b
+
+print(addSum(5,6))
+print(addSum(13,6))
+print(addSum(5,-6))
+print(addSum(-5,2))
+print(addSum(-5,-6))
+
