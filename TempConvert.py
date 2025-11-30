5ffd33484b30f3230160212fde3cf683be750c75
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-11-29 23:07:14 -0800

    Add files via upload

---
+++ TempConvert.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 29 23:03:37 2025
+
+@author: priya
+"""
+
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = input("Choose: ")
+
+if choice == "1":
+    c = float(input("Enter Celsius: "))
+    f = (c * 9/5) + 32
+    print("Fahrenheit:", f)
+elif choice == "2":
+    f = float(input("Enter Fahrenheit: "))
+    c = (f - 32) * 5/9
+    print("Celsius:", c)
+else:
+    print("Invalid choice!")
