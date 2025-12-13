de28df9d6aa0be4a36f6b03c77acbce2b7d3a354
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-12-12 23:52:11 -0800

    Add files via upload

---
+++ evenOddChecker.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 12 23:34:15 2025
+
+@author: priya
+"""
+
+def evenOddChecker(n):
+    if n%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+        
+        
+evenOddChecker(2)
+evenOddChecker(3)
