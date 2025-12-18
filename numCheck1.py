86492d86fef3d410f88ba5bfa09caa5b8b1594be
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2025-12-17 23:54:52 -0800

    Add files via upload

---
+++ numCheck1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 17 23:49:09 2025
+
+@author: priya
+"""
+
+def numCheck(n):
+    if n < 0:
+        print("negative")
+    elif n > 0:
+        print("Positive")
+    else:
+        print("Zero")
+        
+        
+numCheck(-32)
---
+++ rightangletrianglepattern.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 17 23:48:04 2025
+
+@author: priya
+"""
+
+for i in range(8):
+    print(i*'*')
