a454a8de4ea22e41a73c90b48da062ae78e51aef
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2026-01-06 23:53:38 -0800

    Add files via upload

---
+++ checkEvenOrOdd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 23:40:08 2026
+
+@author: priya
+"""
+
+def checkEvenOrOdd(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+        
+        
+checkEvenOrOdd(12)
