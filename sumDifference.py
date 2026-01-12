ecf6488e29f7738a705a1175d1a773e3ba6081ba
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2026-01-11 23:52:45 -0800

    Add files via upload

---
+++ sumDifference.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 11 22:05:33 2026
+
+@author: priya
+"""
+
+def sumDifference():
+    array1 = [1,2,3,4,5,6,7,8,9]
+    oddsum = 0
+    evensum = 0
+    
+    for i in range(len(array1)):
+        if i%2 == 0:
+            evensum = evensum + array1[i]
+        else:
+            oddsum = oddsum + array1[i]
+            
+    return evensum - oddsum
+            
+
+print(sumDifference())
