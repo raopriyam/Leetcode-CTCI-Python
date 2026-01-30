94109bfff9e5f1d9b54ce832aafd85c368ced4c6
Priyam Rao <38253464+raopriyam@users.noreply.github.com>
2026-01-29 22:28:32 -0800

    Add files via upload

---
+++ validPalindrome3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 29 16:43:13 2026
+
+@author: priya
+"""
+
+def validPalindrome(self, s: str) -> bool:
+    l,r = 0,len(s)-1
+
+    while l<r:
+        if s[l] != s[r]:
+            skip_l = s[l+1:r+1]
+            skip_r = s[l:r]
+            return skip_l == skip_l[::-1] or skip_r == skip_r[::-1]
+        l += 1
+        r -= 1
+
+    return True
